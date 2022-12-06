a1cc2752b66aa63178e213c9a0b5dea2f61de805
posholokia <ilya.posholokk@gmail.com>
2022-12-06 19:26:48 +0600

    added apps in settings

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # добавленные приложения
+    'news.apps.NewsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
