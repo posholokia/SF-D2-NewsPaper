6e6baa8db888116a2aee27c8bcdfaa010c3f016d
posholokia <ilya.posholokk@gmail.com>
2023-01-12 22:08:50 +0600

    create links

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    path('news/<int:pk>/', ViewNews.as_view(), name='post_detail'),
    path('news/search/', SearchNews.as_view(), name='post_detail'),
    path('news/create/', PostCreate.as_view(), name='post_create'),
-   path('news/<int:pk>/update/', PostUpdate.as_view(), name='post_create'),
+   path('news/<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
    path('news/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 
 ]
